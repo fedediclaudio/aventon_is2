028df33c48a4372ad814ca2718385a89bf2d8a60
blasNohablas <blasbutera69@gmail.com>
2018-05-17 21:35:29 -0300

    Reorganizacion de codigo

--- index.php
+++ index.php
@@ -9,30 +9,8 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="jquery-3.3.1.min.js"></script>
-	<script>
-		function cargardatos(){
-			//document.getElementById("viajes").innerHTML = "Hello JavaScript"
-			$("#loader").html("<img src='loader2.gif'/>");
-			$.get("cargarviajes.php?pagina=" + pagina,
-			function(data){
-				if (data != "") {
-					$("#viajes").append(data); 
-				}
-				//$('#loader').empty();
-			});
-		}
-		var pagina=0;
-		$(document).ready(function() {
-				cargardatos();	
-		});
-		$(window).scroll(function(){
- 			if ($(window).scrollTop() == $(document).height() - $(window).height()){
-				pagina++;
-				cargardatos()
-			}					
-		});
-	</script>
+  <script type="text/javascript" src="jquery-3.3.1.min.js"></script>
+  <script src="scripts.js/cargadeviajes.js"></script>
 </head>
 <body>
 		<!-- Barra navegacion-->
---
+++ scripts.js/cargadeviajes.js
@@ -0,0 +1,21 @@
+function cargardatos(){
+			//document.getElementById("viajes").innerHTML = "Hello JavaScript"
+			$("#loader").html("<img src='loader2.gif'/>");
+			$.get("cargarviajes.php?pagina=" + pagina,
+			function(data){
+				if (data != "") {
+					$("#viajes").append(data); 
+				}
+				//$('#loader').empty();
+			});
+		}
+		var pagina=0;
+		$(document).ready(function() {
+				cargardatos();	
+		});
+		$(window).scroll(function(){
+ 			if ($(window).scrollTop() == $(document).height() - $(window).height()){
+				pagina++;
+				cargardatos()
+			}					
+		});
