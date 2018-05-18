406932c21994da67bb5da3b61529db66ac689235
blasNohablas <blasbutera69@gmail.com>
2018-05-17 21:02:58 -0300

    Creamos archivo css externo

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="styles.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
@@ -45,7 +46,7 @@
 			}
 		</style>
 </head>
-<body style="background-color:#FAFAFA" >
+<body>
 		<!-- Barra navegacion-->
 		<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #b2ebf2;">
 			<a class="navbar-brand" href="#"><!--<img src="logo.png"/>-->AVENTON</a>
