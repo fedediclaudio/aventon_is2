37bc3d3f34699dca5efc8430c09570495751ec21
pedroodb <dalbianco.pedro@gmail.com>
2018-05-21 17:45:31 -0300

    Uso de columnas para adptabilidad de tarjetas
    Co-Authored-By: fedediclaudio <fedediclaudio@users.noreply.github.com>

--- cargarviajes.php
+++ cargarviajes.php
@@ -10,6 +10,7 @@
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
 				//echo $row["idviaje"] . "<br>";
+    	echo '<div class="col-12 col-md-6 col-lg-4 col-xl-3" style="justify-content:center; display: flex; margin-bottom:10px">';
         echo '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#crearViajeModal" id="botonCartaViaje">';
 				echo '<div class="card">';
 				echo '<div class="card-header">';
@@ -21,6 +22,7 @@
 				echo '</ul>';
 				echo '</div>';
 				echo '</button>';
+				echo '</div>';
     }
 	}
 	
