9cc0f9069b828500e76cc0ad447ed9615ad811c1
pedroodb <dalbianco.pedro@gmail.com>
2018-05-28 19:07:16 -0300

    Gestion de usuarios
    Se agrega sistema de gestion de usuarios
    
    Co-Authored-By: blasb <blasb@users.noreply.github.com>

--- conexionClass.php
+++ conexionClass.php
@@ -31,15 +31,42 @@ class conexion {
 	}
 	
 	function ultimosViajes($pagina) {
-	$conn = $this->establecerConexion();
-	if($conn) {
-		$result = $conn->query("SELECT MAX(idviaje) FROM viaje");
-		$row = mysqli_fetch_assoc($result);
-		$ultimoCargado = $row["MAX(idviaje)"] - ( 20 * $pagina);
-		$ultimoACargar = $ultimoCargado - 20;
-		$result = $conn->query("SELECT * FROM viaje WHERE (idviaje <= " . $ultimoCargado . ") AND (idviaje > " . $ultimoACargar . ") ORDER BY idviaje DESC");
-		return $result;
+		$conn = $this->establecerConexion();
+		if($conn) {
+			$result = $conn->query("SELECT MAX(idviaje) FROM viaje");
+			$row = mysqli_fetch_assoc($result);
+			$ultimoCargado = $row["MAX(idviaje)"] - ( 20 * $pagina);
+			$ultimoACargar = $ultimoCargado - 20;
+			$result = $conn->query("SELECT * FROM viaje WHERE (idviaje <= " . $ultimoCargado . ") AND (idviaje > " . $ultimoACargar . ") ORDER BY idviaje DESC");
+			return $result;
+		}
 	}
+
+	function crearUsuario() {
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			$conn = $this->establecerConexion();
+			if($conn) {
+				$sql = "INSERT INTO aventon.usuario (nombre, apellido, password, descripcion, tarjeta, email, nacionalidad, fecha_nacimiento)
+				VALUES ( '" . $_POST["nombre"] . "', '" . $_POST["apellido"] ."', '" . sha1($_POST["passwd"]) . "', '" . $_POST["descripcion"] . "', '" . $_POST["tarjeta"] . "', '" . $_POST["mail"] . "', '" . $_POST["nacionalidad"] . "', STR_TO_DATE('" . $_POST["fecha_nacimiento"] . "','%Y-%m-%d')" .")";
+				 if (mysqli_query($conn, $sql)) {
+				echo "New record created successfully";
+				} else {
+				echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+				} 
+				$conn->close();
+			} 	
+			else { echo "no";}
+		}
+	}
+
+	function getUsuario($mail, $password){
+		$conn = $this->establecerConexion();
+		if($conn) {
+			$sha1_pass = sha1($password);
+			$sql = "SELECT id FROM usuario WHERE email = '$mail' AND password = '$sha1_pass'";
+			$result=$conn->query($sql);
+			return $result;
+		}
 	}
 }
 ?>
