31f7b240b73629e5bde608fd39ee214be43f570d
fedediclaudio <37820912+fedediclaudio@users.noreply.github.com>
2018-05-29 19:46:46 -0300

    Correccion errores css

--- perfilUsuario.php
+++ perfilUsuario.php
@@ -26,7 +26,7 @@
         include "vistas/navbar.html"
     ?>
     <div  style="margin:5%">
-        <div class="jumbotron p-3 p-md-5 text-black rounded bg-light">
+        <div class="jumbotron p-3 p-md-5 text-black rounded jumbo-infoviaje">
             <div class="row">
                 <div class="col col-12 col-lg-4 px-0">
                     <div>
@@ -35,30 +35,31 @@
                     </div>
                 </div>
                 <div class="col col-12 col-lg-8 px-0"  >
-                    <div class="card" style="width:100%; background-color: #FAFAFA; margin-top: 4px;">
+                    <div class="card card-infoviaje" style="width:100%; margin-top: 4px;">
                         <div class="card-body" style="margin: -1%">
                             <h6 class="card-subtitle mb-2 text-muted">E-mail</h6>
                             <p class="card-text"><?php echo $user['email']?></p>
                         </div>
                     </div>
-                    <div class="card" style="width:100%; background-color: #FAFAFA; margin-top: 4px;">
+                    <div class="card card-infoviaje" style="width:100%; margin-top: 4px;">
                         <div class="card-body" style="margin: -1%">
                             <h6 class="card-subtitle mb-2 text-muted">Fecha de nacimiento</h6>
                             <p class="card-text"><?php echo $user['fecha_nacimiento']?></p>
                         </div>
                     </div>
-                    <div class="card" style="width:100%; background-color: #FAFAFA; margin-top: 4px;">
+                    <div class="card card-infoviaje" style="width:100%; margin-top: 4px;">
                         <div class="card-body" style="margin: -1%">
                             <h6 class="card-subtitle mb-2 text-muted">Nacionalidad</h6>
                             <p class="card-text"><?php echo $user['nacionalidad']?></p>
                         </div>
                     </div>
-                    <div class="card" style="width:100%; background-color: #FAFAFA; margin-top: 4px;">
+					<?php if($user['descripcion'] != ''){ echo '
+                    <div class="card card-infoviaje" style="width:100%; margin-top: 4px;">
                         <div class="card-body" style="margin: -1%">
                             <h6 class="card-subtitle mb-2 text-muted">Descripcion</h6>
-                            <p class="card-text"><?php echo $user['descripcion']?></p>
+                            <p class="card-text">' . $user['descripcion'] . '</p>
                         </div>
-                    </div>
+                    </div>';} ?>
                 </div>
             </div>
         </div>
