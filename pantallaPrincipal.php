a8c66bb6eafdfc99d778f2d9a035f1724012ae30
blasNohablas <blasbutera69@gmail.com>
2018-05-29 15:28:46 -0300

    Cambiar imagen add
    Co-Authored-By: pedroodb <pedroodb@users.noreply.github.com>

--- pantallaPrincipal.php
+++ pantallaPrincipal.php
@@ -22,9 +22,9 @@
 	<div id="viajes" class="row">
         <div class="col-12 col-md-6 col-lg-4 col-xl-3" style="justify-content:center; display: flex;" >
             <button type="button" class="btn btn-light " id="botonCartaViaje" data-toggle="modal" data-target="#crearViajeModal" id="boton-crearViaje; widght:100%; height: 100%">
-                    <div class="card" style="background-color: lightblue;" >
+                    <div style="background-color: #FAFAFA;" >
                         <div style="  ">
-                            <h1 class="display-3">+</h1>
+                            <h1 class="display-3"><img src="boton_mas.png"></h1>
                         </div>
                     </div>
             </button>
