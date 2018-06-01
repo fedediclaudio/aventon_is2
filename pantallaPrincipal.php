406b6ce6dba979eeb8fb6d52c203de977121c61d
fedediclaudio <37820912+fedediclaudio@users.noreply.github.com>
2018-06-01 13:13:50 -0300

    Incluision de bootstrap

--- pantallaPrincipal.php
+++ pantallaPrincipal.php
@@ -7,15 +7,17 @@
   <title>Aventon</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="styles.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+  <script src="bootstrap/jquery.min.js"></script>
+  <script src="bootstrap/popper.min.js"></script>
+  <script src="bootstrap/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+	
   <script type="text/javascript" src="jquery-3.3.1.min.js"></script>
   <script src="scripts.js/cargadeviajes.js"></script>
+  <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body class="body-general">
+  	
 	<!-- Navbar -->
 	<?php
 		include "vistas/navbar.html"
@@ -39,5 +41,6 @@
 	<?php
 		include "vistas/modalCrearViaje.html"
 	?>
+
 </body>
 </html>
--- perfilUsuario.php
+++ perfilUsuario.php
@@ -6,11 +6,11 @@
     <title>Informacion de viaje</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+      <script src="bootstrap/jquery.min.js"></script>
+  <script src="bootstrap/popper.min.js"></script>
+  <script src="bootstrap/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="styles.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="jquery-3.3.1.min.js"></script>
 </head>
 <body>
