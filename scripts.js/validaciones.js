7c5614c0fc2c83577cf26d7e6a50a52c238d29aa
Pedro <dalbianco.pedro@gmail.com>
2018-06-11 21:17:49 -0300

    Buggeada la fecha

--- scripts.js/validaciones.js
+++ scripts.js/validaciones.js
@@ -1,11 +1,30 @@
-function validarMail()
+function validarMail(){
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.getElementById('mailInput').value))
   {
-    console.log(document.getElementById('mailInput').value);
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.getElementById('mailInput').value))
-    {
-      document.getElementById('formularioViaje').setAttribute('action',"../crearUsuario.php")
-      return (true)
-    }
-      alert("Se ha ingresado un mail erroneo")
-      return (false)
+    document.getElementById('formularioViaje').setAttribute('action',"../crearUsuario.php")
+    return (true)
+  } else {
+    return (false)
   }
+}
+
+function validarFechaNacimiento(){
+  var fecha = document.getElementById('inputFechaNacimiento').value;
+  console.log($("#inputFechaNacimiento"));
+  var anio = parseInt(fecha[2],10);
+  var mes = parseInt(fecha[1],10);
+  var dia = parseInt(fecha[0],10);
+  return isDate18orMoreYearsOld(dia,mes,anio);
+}
+
+function isDate18orMoreYearsOld(day, month, year){
+  return new Date(year+18, month-1, day) <= new Date();
+}
+
+function validarRegistro(){
+  if(validarMail() && validarFechaNacimiento()){
+    document.getElementById('buttonCrear').disabled = false;
+  } else {
+    document.getElementById('buttonCrear').disabled = true;
+  }
+}
