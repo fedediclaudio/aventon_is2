c26ac44c3003c3fdd06efae53436f9d1b7215fea
fedediclaudio <37820912+fedediclaudio@users.noreply.github.com>
2018-06-29 15:11:17 -0300

    Mejoras en la vista de postulaciones

--- informacionViaje.php
+++ informacionViaje.php
@@ -64,79 +64,104 @@
                       </div>';} ?>
                 </div>
                 <div class="col-12" style="margin-top:20px">
-                  <?php
+                  
+                </div>
+            </div>
+        </div>
+				<?php
                   if($_SESSION['id']==$viaje["id"]){
-                    echo '<div class="jumbotron" style=" border:1px; border-style:solid; border-color:rgb(13, 71, 161)">
-                      <h4>Postulaciones pendientes</h4>';
+                    echo '<div class="jumbotron p-3 p-md-5 text-black rounded jumbo-infoviaje" style=" border:1px; border-style:solid; border-color:rgb(13, 71, 161)">
+                      <h4 class="h4">Postulaciones pendientes</h4>';
                       $result = $conn->participacionesEnViajeConEstado($viaje["idviajeConcreto"],'pendiente');
                       if (mysqli_num_rows($result) == 0) {
-               		       echo "No hay postulaciones pendientes en este viaje";
+               		       echo '<p class="lead">No hay postulaciones pendientes en este viaje</p>';
             		      } else {
+												echo '<div class="row">';
                         while ($row = mysqli_fetch_assoc($result)) {
                           $user = mysqli_fetch_assoc($conn->getUsuarioPorId($row["idusuario"]));
-                          echo '<div class="card card-infoviaje" style="width:100%; margin-top: 4px;">
-                          <div class="card-body" style="margin: -1%">
-                          <h6 class="card-subtitle mb-2 text-muted">Usuario</h6>
-                          <p class="card-text">' . $user["nombre"] . " " . $user["apellido"] . '</p>';
-                          if(mysqli_num_rows($conn->participacionesEnViajeConEstado($viaje["idviajeConcreto"],'aceptado')) >= $viaje["cantidadAsientos"]){
-                              echo '<div class="alert alert-danger" role="alert">
-                              El viaje esta completo, no puedes aceptar mas postulaciones
-                              </div>';
-                          } else {
-                            echo '<button type="button"onclick="location=\'cambiarEstadoDePostulacion.php?idviaje='.$viaje["idviajeConcreto"].'&idpostulacion='.$row["idparticipacion"].'&estado=aceptado\'"class="btn" style="border-color:rgb(13, 71, 161)">Aceptar postulación</button>
-                            <button type="button"onclick="location=\'cambiarEstadoDePostulacion.php?idviaje='.$viaje["idviajeConcreto"].'&idpostulacion='.$row["idparticipacion"].'&estado=rechazado\'"class="btn" style="border-color:rgb(13, 71, 161)">Rechazar postulación</button>';
-                          }
-                          echo '</div>
-                          </div>';
+                          echo '
+																<div class="col col-12 col-lg-6" style="padding-left:10px; padding-right:10px">
+																	<div class="card card-infoviaje" style="width:100%; margin-top: 4px;">
+	                          				<div class="card-body" style="margin: -1%">
+																			<div class="row">
+																				<div class="col col-7" style="display: flex; align-items: center ">
+                          								<p class="card-text">' . $user["nombre"] . " " . $user["apellido"] . '</p>';
+																				echo '</div>';
+																					if(mysqli_num_rows($conn->participacionesEnViajeConEstado($viaje["idviajeConcreto"],'aceptado')) >= $viaje["cantidadAsientos"]){
+                              						echo '<div class="alert alert-warning" role="alert">
+																									El viaje esta completo, no puedes aceptar mas postulaciones
+																								</div>';
+																					} else {
+																					
+																			  echo '<div class="col col-2" style="padding:2px; padding-left:30px">
+																					<button type="button" onclick="location=\'cambiarEstadoDePostulacion.php?idviaje='.$viaje["idviajeConcreto"].'&idpostulacion='.$row["idparticipacion"].'&estado=aceptado\'"class="btn btn-outline-success" style="color: black; border-color:#BDBDBD; margin: auto; display: block;">Aceptar</button>
+																				</div>
+																				<div class="col col-2" style="padding:2px;padding-left:30px">
+																					<button type="button" onclick="location=\'cambiarEstadoDePostulacion.php?idviaje='.$viaje["idviajeConcreto"].'&idpostulacion='.$row["idparticipacion"].'&estado=rechazado\'" class="btn btn-outline-danger" style="color: black; border-color:#BDBDBD; margin: auto; display: block;">Rechazar</button>
+																				</div>';
+                          							}
+																					echo 			'
+																			</div>
+																		</div>
+																	</div>
+															 	</div>
+															';
                         }
+												echo '</div>';
                       }
+										
                       $result = $conn->participacionesEnViajeConEstado($viaje["idviajeConcreto"],'aceptado');
                       if (!(mysqli_num_rows($result) == 0)) {
-                        echo '<h4>Postulaciones aceptadas</h4>';
+                        echo '<br><h4 class="h4">Postulaciones aceptadas</h4>
+															<div class="row">';
                         while ($row = mysqli_fetch_assoc($result)) {
                           $user = mysqli_fetch_assoc($conn->getUsuarioPorId($row["idusuario"]));
-                          echo '<div class="card card-infoviaje" style="width:100%; margin-top: 4px;">
-                          <div class="card-body" style="margin: -1%">
-                          <h6 class="card-subtitle mb-2 text-muted">Usuario</h6>
-                          <p class="card-text">' . $user["nombre"] . " " . $user["apellido"] . '</p>
-                          </div>
-                          </div>';
+                          echo ' <div class="col col-12 col-lg-6"> 
+																	<div class="card card-infoviaje" style="width:100%; margin-top: 4px;">
+                          				<div class="card-body" style="margin: -1%">
+                          					<h6 class="card-subtitle mb-2 text-muted">Usuario</h6>
+                          					<p class="card-text">' . $user["nombre"] . " " . $user["apellido"] . '</p>
+                          				</div>
+																	</div>
+                          			</div>';
                         }
+												echo '</div>';
                       }
                       $result = $conn->participacionesEnViajeConEstado($viaje["idviajeConcreto"],'rechazado');
                       if (!(mysqli_num_rows($result) == 0)) {
-                        echo '<h4>Postulaciones rechazadas</h4>';
+                        echo '<br><h4 class="h4">Postulaciones rechazadas</h4>
+															<div class="row ">';
                         while ($row = mysqli_fetch_assoc($result)) {
                           $user = mysqli_fetch_assoc($conn->getUsuarioPorId($row["idusuario"]));
-                          echo '<div class="card card-infoviaje" style="width:100%; margin-top: 4px;">
-                          <div class="card-body" style="margin: -1%">
-                          <h6 class="card-subtitle mb-2 text-muted">Usuario</h6>
-                          <p class="card-text">' . $user["nombre"] . " " . $user["apellido"] . '</p>
-                          </div>
-                          </div>';
+                          echo '<div class="col col-12 col-lg-6"> 
+																	<div class="card card-infoviaje" style="width:100%; margin-top: 4px;">
+                          				<div class="card-body" style="margin: -1%">
+                          					<h6 class="card-subtitle mb-2 text-muted">Usuario</h6>
+																		<p class="card-text">' . $user["nombre"] . " " . $user["apellido"] . '</p>
+                          				</div>
+																	</div>
+																</div>';
                         }
+												echo '</div>';
                       }
                     echo '</div>';
                   } else {
                     $result = $conn->participacionesEnViajeDeUsuario($viaje["idviajeConcreto"],$_SESSION["id"]);
                     if(mysqli_num_rows($result) > 0){
                       $postulacion = mysqli_fetch_assoc($result);
-                      echo "<div class=\"alert alert-danger\" role=\"alert\">
+                      echo "<div class=\"alert alert-info\" role=\"alert\">
                       Tu postulación para este viaje se encuentra " . $postulacion["estado"] . "</div>";
                     } else {
                       if(mysqli_num_rows($conn->participacionesEnViajeConEstado($viaje["idviajeConcreto"],'aceptado')) < $viaje["cantidadAsientos"]){
                         echo "<button type=\"button\"onclick=\"location='postularAViaje.php?idviajeConcreto=".$viaje["idviajeConcreto"]."'\"class=\"btn\" style=\"border-color:rgb(13, 71, 161); float:right\">Postularse</button>";
                       } else {
-                        echo '<div class="alert alert-danger" role="alert">
+                        echo '<div class="alert alert-warning" role="alert">
                         El viaje esta completo
                         </div>';
                       }
                     }
                   }
                   ?>
-                </div>
-            </div>
-        </div>
         <div class="row mb-6">
             <div class="col-md-6">
                 <div class="jumbotron p-3 p-md-5 text-black rounded jumbo-infoviaje" id="infoConductor">
